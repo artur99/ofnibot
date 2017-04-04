8abb92ec0fc5b2e11875091d23ec5fde9c3711fd
artur99 <david1989mail@gmail.com>
2017-04-04 20:36:39 +0300

    First commit

---
+++ app/routers.php
@@ -0,0 +1,8 @@
+<?php
+
+// Router list
+// $app->mount('/', new Controllers\SectController());
+
+$app->mount('/', new Controllers\IndexController());
+$app->mount('/demo', new Controllers\DemoController());
+$app->mount('/app', new Controllers\MainController());
